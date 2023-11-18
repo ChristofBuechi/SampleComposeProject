ad3dbfd93e49b06143232ff726905a08bae5368e
Christof <christof.buechi@gmail.com>
2023-11-18 10:28:42 +0100

    update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm")
     // https://github.com/JetBrains/compose-jb/releases
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev686"
+    id("org.jetbrains.compose") version "1.5.10"
 }
 
 dependencies {
